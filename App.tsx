887fb5c457678c6563ced41548841c63e2890210
nitishkumar-shiv <nitishkumar.shivnandan@rently.com>
2025-06-12 17:24:57 +0530

    latest

--- App.tsx
+++ App.tsx
@@ -10,12 +10,51 @@ import './shim.js';
 // @ts-ignore
 import SmartHomeSDK from '@rentlydev/smarthome-sdk';
 
-const getPropertyLevelTokenApi = async ({propertyId, clientId, clientSecret, setAccessToken} :any) => {
+const getPropertyLevelTokenApi = async ({propertyId, agentToken} :any) => {
+
+  console.log('getPropertyLevelTokenApi called with propertyId: ', propertyId, ' and agentToken: ', agentToken);
+
   const url = `https://app2.keyless.rocks/api/properties/${propertyId}/token`;
 
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${agentToken}`,
+      },
+      //body: JSON.stringify(params),
+    });
+
+    console.log('Response status: ', response);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const accessTokenData = await response.json();
+    return Promise.resolve(accessTokenData);
+
+  } catch (error : any) {
+
+    if(error){
+      console.log("Error in fetching property token", error);
+      return Promise.reject(error);
+    }
+
+  }
+};
+
+const getAgentTokenApi = async ({username, password}: any) => {
+
+  console.log('getAgentTokenApi called with username: ', username, ' and password: ', password);
+  const url = `https://app2.keyless.rocks/api/agents`;
+
   const params = {
-    client_id: clientId,
-    client_secret: clientSecret,
+    username: username,
+    password: password,
+     grant_type: 'password',
   };
 
   try {
@@ -33,26 +72,27 @@ const getPropertyLevelTokenApi = async ({propertyId, clientId, clientSecret, set
 
     const accessTokenData = await response.json();
     return Promise.resolve(accessTokenData);
-
-  } catch (error : any) {
-
-    if(error){
-      console.log("Error in fetching property token", error);   
-      return Promise.reject(error); 
+  } catch (error: any) {
+    if (error) {
+      console.log('Error in fetching agent token', error);
+      return Promise.reject(error);
     }
-
   }
-}
+};
 
 
 const App: () => ReactNode = () => {
-  
-  const clientId = "xYu8CZHJRHxCiTmz6gsVCo-FnzKPg5EcE89eWwxjzMc";
-  const clientSecret = "OO7I1CyQI1N9DLNpkrKQGgYBofpXLZaeDi5r79LbXLk";
-  const propertyId = 223439;
+
+  //const clientId = "xYu8CZHJRHxCiTmz6gsVCo-FnzKPg5EcE89eWwxjzMc";
+  //const clientSecret = "OO7I1CyQI1N9DLNpkrKQGgYBofpXLZaeDi5r79LbXLk";
+  const username = 'rakesh+inst@rently.com';
+  const password = 'Rakesh88';
+  const propertyId = 231251;
 
   const getAccessToken = async () =>{
-   return await getPropertyLevelTokenApi( { clientId, clientSecret, propertyId } );            
+    const {access_token} = await getAgentTokenApi({username, password});
+    console.log('Agent Token: ', access_token);
+    return await getPropertyLevelTokenApi({propertyId, agentToken: access_token});
   };
 
   return (
